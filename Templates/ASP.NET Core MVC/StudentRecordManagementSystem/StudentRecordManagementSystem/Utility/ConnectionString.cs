ab4d28a5f88ed23928488256d49d6c78b7207b99
Qliphort Shell <nhoangkiet35@gmail.com>
2024-06-14 10:37:40 -0500

    student record

---
+++ Templates/ASP.NET Core MVC/StudentRecordManagementSystem/StudentRecordManagementSystem/Utility/ConnectionString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentRecordManagementSystem.Utility
+{
+    public class ConnectionString
+    {
+        private const string V = "User Id=sa;Password=12345";
+        private static string cName = "Data Source=.;Initial Catalog=StudentManagement;" + V;
+        public static string CName
+        {
+            get => cName;
+        }
+    }
+}
