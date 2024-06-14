9e261227ec34d608f6c13d2b9c56db102e27f10f
Qliphort Shell <nhoangkiet35@gmail.com>
2024-06-14 14:22:41 -0500

    interface actors

---
+++ Projects/ASP.NET Core MVC/TicketsForMovies/TicketsForMovies/Data/Services/IActorsServices.cs
@@ -0,0 +1,13 @@
+﻿using TicketsForMovies.Models;
+
+namespace TicketsForMovies.Data.Services
+{
+    public interface IActorsServices
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}
