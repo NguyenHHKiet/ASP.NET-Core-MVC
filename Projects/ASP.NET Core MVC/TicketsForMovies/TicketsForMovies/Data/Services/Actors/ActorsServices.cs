1c5f35577eeabe36d40f0ab24813c6bd0837eda3
Qliphort Shell <nhoangkiet35@gmail.com>
2024-06-16 18:04:09 -0500

    actors

---
+++ Projects/ASP.NET Core MVC/TicketsForMovies/TicketsForMovies/Data/Services/Actors/ActorsServices.cs
@@ -0,0 +1,12 @@
+﻿using TicketsForMovies.Data.Base;
+using TicketsForMovies.Models;
+
+namespace TicketsForMovies.Data.Services.Actors
+{
+    public class ActorsServices : EntityBaseRepository<Actor>, IActorsServices
+    {
+        public ActorsServices(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Projects/ASP.NET Core MVC/TicketsForMovies/TicketsForMovies/Data/Services/Actors/IActorsServices.cs
@@ -0,0 +1,9 @@
+﻿using TicketsForMovies.Data.Base;
+using TicketsForMovies.Models;
+
+namespace TicketsForMovies.Data.Services.Actors
+{
+    public interface IActorsServices : IEntityBaseRepository<Actor>
+    {
+    }
+}
